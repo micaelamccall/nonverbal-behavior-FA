aee3631f8e6579eb98fe1b80e1ede087d5499285
micaelamccall <mica2014@comcast.net>
2019-09-14 18:32:50 -0400

    add setup.R

---
+++ setup.R
@@ -0,0 +1,20 @@
+`%notin%` <- Negate(`%in%`)
+if (proj_version != version$version.string){
+  print(paste("You have ", version$version.string, ". This project was created with R version 3.5.2 (2018-12-20). Some packages may not install or work properly"))
+} else {
+  print("You have the same version of R as was used in this project")
+}
+
+for (i in 1:length(req)){
+  pkg<-req[[i]]
+  if (pkg %notin% rownames(installed.packages())){
+    install.packages(pkg)
+  }
+  if (pkg %in% rownames(installed.packages()) & pkg %notin% loadedNamespaces()){
+    library(pkg, character.only = T)
+    print(paste("Attaching package:", pkg))
+  }
+  if (pkg %notin% rownames(installed.packages())){
+    print(paste("Error installing ", pkg, ". Check Warnings."))
+  }
+}
